10142372e788ef593a0a7c321f3dc0fc8c0d1052
YashMunjal <yashmu45@gmail.com>
2019-12-10 02:50:58 +0530

    1st

---
+++ js/main.js
@@ -0,0 +1,12 @@
+var typed = new Typed('#typed',{
+    strings:['Rooms','Laundry','Dining'],
+    typeSpeed:140,
+    backSpeed: 110,
+    backDelay:700,
+    loop: true,
+     loopCount: Infinity
+})
+AOS.init({
+    duration: 1500,
+  })
+  
