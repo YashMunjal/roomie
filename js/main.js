a682ca13e3279adf9da1fd9e34d7ca0ab3cec8e2
YashMunjal <yashmu45@gmail.com>
2019-12-16 02:59:20 +0530

    major changes

--- js/main.js
+++ js/main.js
@@ -1,12 +1,51 @@
-var typed = new Typed('#typed',{
-    strings:['Rooms','Laundry','Dining'],
-    typeSpeed:140,
-    backSpeed: 110,
-    backDelay:700,
-    loop: true,
-     loopCount: Infinity
-})
+
 AOS.init({
-    duration: 1500,
-  })
-  
+  duration: 1500,
+})
+
+$(document).ready(function () {
+  $('.flat-block').hide();
+  $('.independent-block').hide();
+
+  $('.prop-one').click(function () {
+    $('.prop-one').addClass('prop-selected');
+
+    if ($('.prop-two').hasClass('prop-selected')) {
+      $(('.prop-two').removeClass('prop-selected'));
+    }
+    if ($('.prop-three').hasClass('prop-selected')) {
+      $('.prop-three').removeClass('prop-selected');
+    }
+
+    $('.independent-block').hide();
+    $('.flat-block').hide();
+    $('.pg-block').show();
+  });
+
+  $('.prop-two').click(function () {
+    $('.prop-two').addClass('prop-selected');
+    if ($(".prop-one").hasClass('prop-selected')) {
+      $('.prop-one').removeClass('prop-selected');
+    }
+    if ($('.prop-three').hasClass('prop-selected')) {
+      $(this).removeClass('prop-selected');
+    }
+    $('.independent-block').hide();
+    $('.pg-block').hide();
+    $('.flat-block').show();
+  });
+
+  $('.prop-three').click(function () {
+    $('.prop-three').addClass('prop-selected');
+
+    if ($(".prop-two").hasClass('prop-selected')) {
+      $('.prop-two').removeClass('prop-selected');
+    }
+    if ($('.prop-one').hasClass('prop-selected')) {
+      $(this).removeClass('prop-selected')
+    }
+    $('.pg-block').hide();
+    $('.flat-block').hide();
+    $('.independent-block').show();
+  });
+});
